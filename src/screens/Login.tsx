25bf15232fae471d0f65e56cdcc502dbd8201e34
Hoàng Anh <nth.anh020209@gmail.com>
2024-10-10 00:00:14 +0700

    fix ux/ui

--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -1,6 +1,12 @@
-import { FaRegUser } from 'react-icons/fa';
-import { CiFacebook } from 'react-icons/ci';
-import { CiLock } from 'react-icons/ci';
+import {
+  FaUser,
+  FaGithub,
+  FaFacebook,
+  FaTiktok,
+  FaYoutube,
+  FaLock,
+} from 'react-icons/fa';
+import img from '../assets/react.svg';
 
 const Login = () => {
   return (
@@ -8,19 +14,17 @@ const Login = () => {
       {/* Thẻ div trái */}
       <div className='bg-white relative w-4/5 h-screen rounded-tr-[400px] rounded-br-[400px] flex items-center justify-center'>
         <div>
-          <img
-            src='https://www.freepik.com/free-vector/general-business-horizontal-banner-template_13914232.htm#fromView=search&page=1&position=6&uuid=230ac39d-0b7d-4102-ae09-7359a49db816'
-            alt=''
-            className='w-[520px] h-[80px] shadow-lg mb-10 rounded-xl'
-          />
+          <div className='shadow-2xl mb-10 rounded-xl p-2'>
+            <img src={img} className='w-[500px] h-[80px]' />
+          </div>
 
-          <p className='font-bold text-2xl text-center text-[#62748c]'>
+          <p className='font-bold text-2xl text-center text-[#62748c] mb-4'>
             Đăng nhập
           </p>
 
-          <div className='flex items-center gap-2 mb-2'>
-            <FaRegUser />
-            <p>Username</p>
+          <div className='flex items-center gap-2 mb-2 text-[#64728c]'>
+            <FaUser size={'12px'} />
+            <p className='text-sm'>Tên đăng nhập</p>
           </div>
 
           <input
@@ -29,13 +33,13 @@ const Login = () => {
             className='border rounded-xl w-full p-2 mb-2'
           />
 
-          <div className='flex items-center gap-2  mb-2'>
-            <CiLock />
-            <p>Password</p>
+          <div className='flex items-center gap-2 mb-2 text-[#64728c]'>
+            <FaLock size={'12px'} />
+            <p className='text-sm'>Mật khẩu</p>
           </div>
 
           <input
-            type='text'
+            type='password'
             placeholder='Mật khẩu'
             className='border rounded-xl w-full p-2 mb-5'
           />
@@ -45,24 +49,32 @@ const Login = () => {
           </button>
 
           <a href='#' className='text-[#ff7b1d] text-center w-full block mt-4'>
-            Quên mật khẩu
+            Quên mật khẩu?
           </a>
 
-          <div className='flex gap-2 absolute left-0 bottom-0 p-4'>
-            <CiFacebook size={'30px'} color='#62748c' />
-            <CiFacebook size={'30px'} color='#62748c' />
-            <CiFacebook size={'30px'} color='#62748c' />
-            <CiFacebook size={'30px'} color='#62748c' />
+          <div className='flex gap-2 absolute left-0 bottom-0 p-4 '>
+            <a href='#'>
+              <FaFacebook size={'20px'} color='#62748c' />
+            </a>
+            <a href='#'>
+              <FaGithub size={'20px'} color='#62748c' />
+            </a>
+            <a href='#'>
+              <FaTiktok size={'20px'} color='#62748c' />
+            </a>
+            <a href='#'>
+              <FaYoutube size={'20px'} color='#62748c' />
+            </a>
           </div>
 
-          <div className='w-[180px] h-[180px] bg-[#f6f5fb] rounded-full absolute right-[-90px] top-1/2 flex items-center justify-center'>
-            <p>hehe</p>
+          <div className='w-[180px] h-[180px] bg-[#f6f5fb] rounded-full absolute right-[-90px] top-1/2 flex items-center justify-center -translate-y-2/4'>
+            <img src={img} alt='' className='w-20' />
           </div>
         </div>
       </div>
 
       {/* Thẻ div phải */}
-      <div className='bg-[#ff7b1d] w-1/5 h-screen'>2</div>
+      <div className='bg-[#ff7b1d] w-1/5 h-screen'></div>
     </div>
   );
 };
