9f85624601dfbc6ccd7c52fe8156574d28723373
Hoàng Anh <nth.anh020209@gmail.com>
2024-10-18 10:52:50 +0700

    check login status

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
+import { useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
 import Layout from './components/Layout';
 import './App.css';
 
 function App() {
-  return (
-    <>
-      <Layout />
-    </>
-  );
+  const nav = useNavigate();
+
+  useEffect(() => {
+    const checkLoginStatus = async () => {
+      try {
+        const token = await localStorage.getItem('authToken');
+        if (token === null) {
+          nav('/login');
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    checkLoginStatus();
+  }, []);
+
+  return <Layout />;
 }
 
 export default App;
