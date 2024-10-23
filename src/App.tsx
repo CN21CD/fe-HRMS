5c5142159dc234d1c2492043717d3968312bf1ad
Hoàng Anh <nth.anh020209@gmail.com>
2024-10-23 23:44:55 +0700

    fix bug deploy render

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import { useNavigate } from 'react-router-dom';
-import { useEffect } from 'react';
+// import { useNavigate } from 'react-router-dom';
+// import { useEffect } from 'react';
 import Layout from './components/Layout';
 import './App.css';
 
